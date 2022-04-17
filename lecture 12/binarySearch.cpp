658c7aca4e243bdac82f0e95064dfcce29f021b9
a092devs <arsalanakhtar0@gmail.com>
2022-04-17 15:07:01 +0530

    added - binary search

---
+++ lecture 12/binarySearch.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int binarySearch(int arr[], int n, int key){
+    int start = 0, end = n-1;
+    int mid = start + (end-start)/2;
+
+    while(start <= end){
+        if(arr[mid] == key){
+            return mid;
+        }
+        if(key > arr[mid]){
+            start = mid + 1;
+        }
+        else{
+            end = mid - 1;
+        }
+        mid = start + (end-start)/2;
+    }
+    return -1;
+}
+
+int main(){
+    int n, arr[100], key;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    cout << "Enter the elements of the array: ";
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    cout << "Enter the element to search for: ";
+    cin >> key;
+
+    cout << "Index of " << key << " is: " << binarySearch(arr, n, key) << endl;
+
+    return 0;
+}
