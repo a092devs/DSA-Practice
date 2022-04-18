9ee082aa5aa537a6501343b16e12b0aea5a3080c
a092devs <arsalanakhtar0@gmail.com>
2022-04-18 23:26:50 +0530

    added - locating pivot index - square root with precision

---
+++ lecture 14/getPivot.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int getPivot(int arr[], int n){
+    int s = 0, e = n - 1;
+    int mid = s + (e - s)/2;
+
+    while (s<e){
+        if (arr[mid] >= arr[0]){
+            s = mid + 1;
+        }
+        else{
+            e = mid;
+        }
+        mid = s + (e - s)/2;
+    }
+    return s;
+}
+
+int main(){
+    int arr[100], n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    cout << "Pivot Index is at: " << getPivot(arr, n) << endl;
+
+    return 0;
+}
---
+++ lecture 14/sqrt.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+long long int sqrtInteger(int n){
+    int s = 0, e = n;
+    long long int mid = s + (e-s)/2;
+
+    long long int ans = -1;
+
+    while (s <= e){
+        long long int square = mid*mid;
+        if (square == n){
+            return mid;
+        }
+        else if (square <= n){
+            ans = mid;
+            s = mid + 1;
+        }
+        else
+            e = mid - 1;
+    mid = s + (e-s)/2;
+    }
+    return ans;
+}
+
+double floatPrecision(int n, int k, long double ans){
+    double factor = 1;
+    for (int i = 0; i < k; i++){
+        factor /= 10;
+        for (double j = ans; j*j<n; j+=factor){
+            ans = j;
+        }
+    }
+    return ans;
+}
+
+int main(){
+    int n, k;
+    cout << "Enter the number: ";
+    cin >> n;
+
+    cout << "Enter the precision of decimal points: ";
+    cin >> k;
+
+    int ans = sqrtInteger(n);
+    cout << "The square root of " << n << " is: " << floatPrecision(n, k, ans) << endl; 
+
+    return 0;
+}
