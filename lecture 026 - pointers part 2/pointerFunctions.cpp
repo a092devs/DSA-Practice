e57f1a2846e09372faa5f9b5ad8ab83cd6da76d8
a092devs <arsalanakhtar0@gmail.com>
2022-06-03 21:59:49 +0530

    changes according to my eyes
    🤪

--- lecture 026 - pointers part 2/pointerFunctions.cpp
+++ lecture 026 - pointers part 2/pointerFunctions.cpp
@@ -1,30 +1,31 @@
 #include<iostream>
 using namespace std;
 
-void print(int *p) {
+void print(int *p){
     cout << *p << endl; 
 }
 
-void update(int *p) {
-
+void update(int *p){
     // p = p + 1;
     // cout << "inside "<< p <<endl;
     *p = *p + 1;
-
 }
 
-int getSum(int *arr, int n) {
+int getSum(int *arr, int n){
 
-    cout << endl << "Size: " << sizeof(arr) << endl;
+    cout << "Size of the pointer to the array: " << sizeof(arr) << endl;
 
     int sum = 0;
-    for(int i=0;i<n;i++) {
+    for(int i=0;i<n;i++){
         sum += arr[i];
     }
-    return sum;
+
+    cout << "Sum of the array: " << sum << endl;
+
+    return 0;
 }
 
-int main() {
+int main(){
 
     // int value = 5;
     // int *p = &value;
@@ -36,7 +37,7 @@ int main() {
 
     int arr[6] = {1,2,3,4,5,8};
 
-    cout << "Sum is " << getSum(arr+3 ,3) << endl ;  
+    getSum(arr+3 ,3);
 
     return 0;
 }
